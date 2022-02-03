8d561c45eb8c1d878816bd04323c2d12fa0494cc
Olga Shevchik <olga.shevchik03@gmail.com>
2022-02-03 21:43:39 +0300

    add lesson03

---
+++ app/src/main/java/com/example/emptyproject/lesson03/FizzBuzzWoof.kt
@@ -0,0 +1,12 @@
+package com.example.emptyproject.lesson03
+
+fun main() {
+    for (i in 1..100) {
+        val res = mutableListOf<String>()
+        if (i % 3 == 0) res.add("Fizz")
+        if (i % 5 == 0) res.add("Buzz")
+        if (i % 7 == 0) res.add("Woof")
+        if (res.size == 0) res.add(i.toString())
+        println(res.joinToString(separator=" "))
+    }
+}
---
+++ app/src/main/java/com/example/emptyproject/lesson03/SortedMerge.kt
@@ -0,0 +1,24 @@
+package com.example.emptyproject.lesson03
+
+fun merge(list1: List<Int>, list2: List<Int>): MutableList<Int> {
+    var pos1 = 0
+    var pos2 = 0
+    val result = mutableListOf<Int>()
+    while (pos1 < list1.size || pos2 < list2.size) {
+        if (pos1 < list1.size && (pos2 == list2.size || list1[pos1] < list2[pos2])) {
+            result.add(list1[pos1])
+            pos1++
+        } else {
+            result.add(list2[pos2])
+            pos2++
+        }
+    }
+    return result
+}
+
+fun main() {
+    val list1 = listOf(1, 7, 11, 17)
+    val list2 = listOf(3, 5, 13)
+    val mergedList = merge(list1, list2)
+    println(mergedList)
+}
