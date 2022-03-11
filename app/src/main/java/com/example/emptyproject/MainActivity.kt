1799ead74667e8c90457b2680694da531a95f878
Olga Shevchik <olga.shevchik03@gmail.com>
2022-03-11 14:58:53 +0300

    Add methods for fragments

--- app/src/main/java/com/example/emptyproject/MainActivity.kt
+++ app/src/main/java/com/example/emptyproject/MainActivity.kt
@@ -3,10 +3,11 @@ package com.example.emptyproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
+import android.widget.LinearLayout
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
-import androidx.core.view.isInvisible
+import androidx.core.view.isGone
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity(), ListFragment.OnFragmentSendDataListene
     private var toolbar: Toolbar? = null
     private var drawerLayout: DrawerLayout? = null
     private var navigationView: NavigationView? = null
+    private var listContainer : LinearLayout? = null
+    private var mainContainer : FrameLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +27,14 @@ class MainActivity : AppCompatActivity(), ListFragment.OnFragmentSendDataListene
         val mainScreenFragment = MainScreenFragment()
         val listFragment = ListFragment()
         val calculatorFragment = CalculatorFragment()
-        val editFragment = EditFileFragment()
+        listContainer = findViewById(R.id.list_container)
+        mainContainer = findViewById(R.id.fragment_main_screen)
+
         drawerLayout = findViewById(R.id.drawer_layout)
         toolbar = findViewById(R.id.toolbar)
         navigationView = findViewById(R.id.navigation_view)
 
         val toolbar = toolbar ?: return
-        val drawerLayout = drawerLayout ?: return
-        val navigationView = navigationView ?: return
 
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.fragment_main_screen, mainScreenFragment)
@@ -39,9 +42,14 @@ class MainActivity : AppCompatActivity(), ListFragment.OnFragmentSendDataListene
             commit()
         }
 
-        toolbar.title = "Мультифунк"
+        toolbar.title = getString(R.string.main_screen_title)
 
-        // Initialize the action bar drawer toggle instance
+        initializeActionBarDrawerToggle()
+        setNavigationDrawerListener(listFragment, calculatorFragment,mainScreenFragment)
+    }
+
+    private fun initializeActionBarDrawerToggle() {
+        val drawerLayout = drawerLayout ?: return
         val drawerToggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
             this,
             drawerLayout,
@@ -50,56 +58,52 @@ class MainActivity : AppCompatActivity(), ListFragment.OnFragmentSendDataListene
             R.string.drawer_close
         ) {}
 
-        // Configure the drawer layout to add listener and show icon on toolbar
         drawerToggle.isDrawerIndicatorEnabled = true
         drawerLayout.addDrawerListener(drawerToggle)
         drawerToggle.syncState()
+    }
 
-        val listContainer = findViewById<FrameLayout>(R.id.list_container)
-        // Set navigation view navigation item selected listener
+    private fun setNavigationDrawerListener(
+        listFragment: ListFragment,
+        calculatorFragment: CalculatorFragment,
+        mainScreenFragment: MainScreenFragment
+    ) {
+        val listContainer = findViewById<LinearLayout>(R.id.list_container)
+        val mainContainer = findViewById<FrameLayout>(R.id.fragment_main_screen)
+        val drawerLayout = drawerLayout ?: return
+        val navigationView = navigationView ?: return
         navigationView.setNavigationItemSelectedListener {
+
             when (it.itemId) {
                 R.id.main ->
                     supportFragmentManager.beginTransaction().apply {
                         if (findViewById<FrameLayout>(R.id.layout_land) != null) {
-                            hide(listFragment)
-                            hide(editFragment)
-                            show(mainScreenFragment)
-                            listContainer.isInvisible = true
-                        } else {
-
+                            mainContainer.isGone = false
+                            listContainer.isGone = true
                         }
                         replace(R.id.fragment_main_screen, mainScreenFragment)
                         commit()
-                        toolbar.title = "Мультифунк"
+                        toolbar?.title = getString(R.string.main_screen_title)
                     }
                 R.id.text_editor -> supportFragmentManager.beginTransaction().apply {
                     if (findViewById<FrameLayout>(R.id.layout_land) == null) {
                         replace(R.id.fragment_main_screen, listFragment)
                     } else {
-                        listContainer.isInvisible = false
-                        hide(mainScreenFragment)
-                        hide(calculatorFragment)
+                        mainContainer.isGone = true
+                        listContainer.isGone = false
                         replace(R.id.fragment_list_container, listFragment)
-                        show(listFragment)
-                        show(editFragment)
                     }
                     commit()
-                    toolbar.title = "Редактирование"
+                    toolbar?.title = getString(R.string.edit_title)
                 }
                 R.id.calculator -> supportFragmentManager.beginTransaction().apply {
-                    if (findViewById<FrameLayout>(R.id.layout_land) == null) {
-
-
-                    } else {
-                        val listContainer = findViewById<FrameLayout>(R.id.list_container)
-                        listContainer.isInvisible = true
-                        hide(listFragment)
-                        show(calculatorFragment)
-                    }
                     replace(R.id.fragment_main_screen, calculatorFragment)
+                    if (findViewById<FrameLayout>(R.id.layout_land) != null) {
+                        listContainer.isGone = true
+                        mainContainer.isGone = false
+                    }
                     commit()
-                    toolbar.title = "Калькулятор"
+                    toolbar?.title = getString(R.string.calculator_title)
                 }
             }
 
@@ -116,16 +120,12 @@ class MainActivity : AppCompatActivity(), ListFragment.OnFragmentSendDataListene
         }
     }
 
-        private fun editFile(args: Bundle) {
+    private fun editFile(args: Bundle) {
         val editFileFragment = EditFileFragment()
-        val listFragment = supportFragmentManager.findFragmentById(R.id.fragment_list_container)
         editFileFragment.arguments = args
         if (findViewById<FrameLayout>(R.id.layout_land) == null) {
             supportFragmentManager.beginTransaction().apply {
                 replace(R.id.fragment_main_screen, editFileFragment)
-                if (listFragment != null) {
-                    hide(listFragment)
-                }
                 addToBackStack(null)
                 commit()
             }
@@ -167,7 +167,7 @@ class MainActivity : AppCompatActivity(), ListFragment.OnFragmentSendDataListene
         if (fileContentFragment != null) {
             if (findViewById<FrameLayout>(R.id.layout_land) == null) {
                 supportFragmentManager.beginTransaction().apply {
-                    replace(R.id.fragment_list_container, listFragment)
+                    replace(R.id.fragment_main_screen, listFragment)
                     if (editFragment != null) {
                         hide(editFragment)
                     }
@@ -182,12 +182,19 @@ class MainActivity : AppCompatActivity(), ListFragment.OnFragmentSendDataListene
         }
     }
 
-    private val hashMapItems =hashMapOf(
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount == 1) {
+            finish()
+            return
+        }
+        super.onBackPressed()
+    }
+
+    private val hashMapItems = hashMapOf(
         R.id.main to "main_screen_tag",
         R.id.text_editor to "files_list_tag",
         R.id.calculator to "calculator_tag"
     )
-
 
     companion object {
         const val IS_NEW_FILE = "isNewFile"
